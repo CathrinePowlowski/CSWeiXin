f66adf6015dca68ddf649a9894091e61b5514ff3
CathrinePowlowski <UNuOOsYt92015@outlook.com>
2023-12-26 13:39:38 +0800

    enim

---
+++ LoginForm.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Threading.Tasks;
+using CCWin.SkinControl;
+using CSWeiXin.Util;
+using CSWeiXin.Weixin;
+
+namespace CSWeiXin
+{
+    public partial class LoginForm : CCWin.Skin_Mac
+    {
+        MainForm mainForm = null;
+
+        public bool ValidationFailure
+        {
+            get;
+            set;
+        } = false;
+
+        public LoginForm()
+        {
+            InitializeComponent();
+            mainForm = new MainForm(this);
+        }
+
+        private void LoginForm_Load(object sender, EventArgs e)
+        {
+            Task.Factory.StartNew(() =>
+            {
+                if (!ValidationFailure && LoginHelper.InitWithData())
+                {
+                    this.BeginInvoke(new Action(() =>
+                    {
+                        try
+                        {
+                            mainForm.Show();
+                            this.Hide();
+                            return;
+                        }
+                        catch { }
+                    }));
+                }
+                this.BeginInvoke(new Action(OnFailed));
+            });
+        }
+
+        private void OnFailed()
+        {
+            Task.Factory.StartNew(() =>
+            {
+                skinAnimator1.WaitAllAnimations();
+
+                skinAnimator1.ShowSync(label1, true, Animation.HorizBlind);
+
+                var uuid = LoginHelper.GetUUID();
+                var image = LoginHelper.GetQR(uuid);
+                new CalcTimeUtil().Start(25, OnChanged);
+
+                if (this.InvokeRequired)
+                {
+                    this.BeginInvoke(new Action(() =>
+                    {
+                        try
+                        {
+                            pictureBox1.Image = image;
+                            skinAnimator1.HideSync(pictureBox1, true, Animation.Particles);
+                            skinAnimator1.WaitAllAnimations();
+                            skinAnimator1.ShowSync(pictureBox1, true, Animation.Mosaic);
+                        }
+                        catch (Exception ex)
+                        {
+                            LogUtil.WriteLog("OnFailed", ex);
+                        }
+                    }));
+                }
+                else
+                {
+                    try
+                    {
+                        pictureBox1.Image = image;
+                        skinAnimator1.HideSync(pictureBox1, true, Animation.Particles);
+                        skinAnimator1.WaitAllAnimations();
+                        skinAnimator1.ShowSync(pictureBox1, true, Animation.Mosaic);
+                    }
+                    catch (Exception ex)
+                    {
+                        LogUtil.WriteLog("OnFailed", ex);
+                    }
+                }
+                LoginHelper.Login(uuid, OnFailed, OnSuccessed);
+            });
+        }
+
+        private void OnSuccessed()
+        {
+            this.BeginInvoke(new Action(() =>
+            {
+                this.Hide();
+                this.ValidationFailure = false;
+                if (mainForm == null || mainForm.IsDisposed)
+                {
+                    mainForm = new MainForm(this);
+                }
+                mainForm.Show();
+            }));
+
+        }
+
+
+        private void OnChanged(int seconds)
+        {
+            this.BeginInvoke(new Action(() =>
+            {
+                label2.Text = seconds.ToString();
+            }));
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CSWeiXin.Util;
+
+namespace CSWeiXin
+{
+    static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            try
+            {
+                ServicePointManager.SecurityProtocol = (SecurityProtocolType)3072;
+                ServicePointManager.MaxServicePoints = 0;
+                ServicePointManager.DefaultConnectionLimit = 1024;
+
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new LoginForm());
+            }
+            catch(Exception ex)
+            {
+                LogUtil.WriteLog("Program.Main", ex);
+                MessageBox.Show("出现严重异常，已记录到日志");
+            }
+
+            
+        }
+    }
+}
